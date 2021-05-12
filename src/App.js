0a5765d7d7ba7810b839ed64194ca3d07db10ed8
AkmalRixsiboyev <70771432+AkmalRixsiboyev@users.noreply.github.com>
2021-05-12 21:19:44 +0500

    add new item

--- src/App.js
+++ src/App.js
@@ -1,31 +1,49 @@
-import React, { useState, useEffect } from 'react'
+import React, {useState, useEffect} from 'react'
 import List from './List'
 import Alert from './Alert'
 
 function App() {
-  const [name,setName]= useState("");
-  const [list,setList]= useState([]);
-  const [isEditing,setIsEditing]= useState(false);
-  const [editID,setEditID]= useState(null);
-  const [alert,setAlert]= useState( {show: false, msg:"", type: "" });
+    const [name, setName] = useState ( "" );
+    const [list, setList] = useState ( [] );
+    const [isEditing, setIsEditing] = useState ( false );
+    const [editID, setEditID] = useState ( null );
+    const [alert, setAlert] = useState ( {show : false, msg : "", type : ""} );
 
-  const handleSubmit=(e)=>{
-    e.preventDefault();
-    if (!name){
+    const handleSubmit = (e) => {
+        e.preventDefault ();
+        if (!name) {
 
-    }else if(name && isEditing){
+        } else if (name && isEditing) {
 
-    }else {
-      const newItem ={id:new Date().getTime().toString(),title:name};
-      setList([...list, newItem]);
-    }
-  };
+        } else {
+            const newItem = {id : new Date ().getTime ().toString (), title : name};
+            setList ( [...list, newItem] );
+            setName ( "" );
+        }
+    };
 
-  return(
-      <section className={'section-center'}>
+    return (
+        <section className={'section-center'}>
+            <form className={'grocery-form'} onSubmit={handleSubmit}>
+                {
+                    alert.show && <Alert/>
 
-      </section>
-  )
+                }
+                <h3>Список Заьетки</h3>
+                <div className="form-control">
+                    <input type="text" className={'grocery'} placeholder={'место для заметки'} value={name}
+                           onChange={(e) => setName ( e.target.value )}/>
+                    <button type={'submit'} className={'submit-btn'}>
+                        {isEditing ? 'Изменить' : 'Добавить'}
+                    </button>
+                </div>
+            </form>
+            <div className="grocery-container">
+                <List items={list}/>
+                <div className="clear-btn"> очистиь </div>
+            </div>
+        </section>
+    )
 }
 
 export default App
